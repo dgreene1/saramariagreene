b2bcbb4b28c64d169aa1d7d69016ef4e6610a1fc
Dan Greene <14020024+dgreene1@users.noreply.github.com>
2023-02-22 11:22:14 -0500

    initial cypress test

---
+++ cypress/e2e/sanity-check.cy.ts
@@ -0,0 +1,20 @@
+describe("sanity check", () => {
+  it("index looks good", () => {
+    const titleOfWebsite = "Sara Maria Greene";
+    const classForSarasEmailLink = "email-element";
+    const sarasEmailAddress = "sara1maria1greene@gmail.com";
+    const sarasEmailLinkValue = `mailto:${sarasEmailAddress}`;
+    cy.visit("/");
+    cy.get("h1").should("have.text", titleOfWebsite);
+    cy.log("Are we on the right page?");
+    cy.get("h2").should("have.text", "About the Author");
+    cy.log("check the email link in the side navigation");
+    cy.get(`header .${classForSarasEmailLink} a`)
+      .should("have.attr", "href", sarasEmailLinkValue)
+      .should("have.text", sarasEmailAddress);
+    cy.log("check the email link in the bio");
+    cy.get(`.wrapper section .${classForSarasEmailLink} a`)
+      .should("have.attr", "href", sarasEmailLinkValue)
+      .should("have.text", sarasEmailAddress);
+  });
+});
