3408dc125d4a0ebc4ebc87599b7b823bc369b6fa
Dan Greene <14020024+dgreene1@users.noreply.github.com>
2023-07-13 14:59:04 -0400

    added other pages and enhanced linter for multiple pages

---
+++ awards-and-honors.md
@@ -0,0 +1,42 @@
+---
+layout: default
+---
+
+<!---
+DO NOT JUST COPY/PASTE IN HERE because it will undo the email obfuscation code you wrote. Make sure to have the email-element span in here
+-->
+
+## Publications, Awards, and Honors
+
+### Publications
+
+[“The Mother Witch,”](https://roommagazine.com/fiction-contest-2022-the-winners/) Room Magazine, Issue 46.3, October 2023
+
+[“Parapsychology,”](https://slipperyelm.findlay.edu/announcing-2023-prose-finalists/) Slippery Elm Journal, 2023 Print Edition
+
+[“Mine,”](https://www.litromagazine.com/usa/2023/04/mine/) Litro Magazine Online, Editor’s Pick Essay, April 15, 2023
+
+[“Bad Cookie,”](https://www.amazon.com/dp/1915247101) Dandelion Years: Bath Flash Fiction Volume Seven, Ad Hoc Fiction, January 25, 2023
+
+[“Misdiagnosis,”](https://www.amazon.com/TulipTree-Review-Winter-Humor-issue/dp/1734969083/ref=sr*1_1?crid=2E0MBAYBVFER1&keywords=tuliptree+review+humor&qid=1688134041&s=books&sprefix=tuliptree+review+humpr%2Cstripbooks%2C81&sr=1-1) TulipTree Review, Fall/Winter 2022 Humor issue, December 17, 2022, Edited by Jennifer Top
+
+[“The Pomelo,”](https://www.npr.org/2013/06/08/187905504/the-pomelo) NPR Three Minute Fiction, NPR, June 8, 2013
+
+[“Themes of Justice, Love, and Nature in Dante Alighieri’s The Divine Comedy,”](https://www.amazon.com/Encyclopedia-Themes-Literature-Facts-Library/dp/0816071616?ref*=ast_author_dp) Encyclopedia of Themes in English
+Literature: Volume 1, Edited by Jennifer McClinton-Temple, Facts on File, Inc., December 1, 2010
+
+### Awards and Honors
+
+Second Place Winner - Room Magazine Short Fiction Contest 2022
+
+Shortlist - Slippery Elm Journal Prose Award 2023
+
+Finalist - Tennessee Williams Very Short Fiction Contest 2023
+
+Shortlist - The Masters Review Flash Fiction Contest 2022
+
+Honorable Mention - TulipTree Review Humor Contest 2022
+
+Longlist - Bath Flash Fiction Award 2022
+
+Finalist - NPR Three Minute Fiction Contest 2013
--- customLinter.js
+++ customLinter.js
@@ -1,125 +1,145 @@
 const fs = require("fs");
 const path = require("path");
+const { globSync } = require("glob");
 
-const indexMDFile = fs.readFileSync(path.join(".", "index.md"), "utf-8");
+const lintAFile = (filenameToLint) => {
+  console.log(`Starting lintAFile for filename: ${filenameToLint}`);
+  fs.readFileSync(filenameToLint, "utf-8");
+  console.log(`Finished reading the file into memory`);
+  const htmlTagsThatDoNotSelfClose = ["a", "p", "div"];
 
-const htmlTagsThatDoNotSelfClose = ["a", "p", "div"];
-
-const getTextWithinATag = (htmlString) => {
-  return htmlString.substring(
-    htmlString.indexOf(">") + 1,
-    htmlString.lastIndexOf("<")
-  );
-};
-
-/**
- *
- * @param {string} textToSearch
- * @param {string} tagName
- */
-const isTagClosed = (textToSearch, tagName) => {
-  const openingTags = textToSearch.match(new RegExp(`<${tagName}`, "g"));
-  const countOfOpeningTags = !!openingTags ? openingTags.length : 0;
-
-  const closingTags = textToSearch.match(new RegExp(`<\/${tagName}`, "g"));
-  const countOfClosingTags = !!closingTags ? closingTags.length : 0;
-
-  return countOfOpeningTags === countOfClosingTags;
-};
-
-htmlTagsThatDoNotSelfClose.forEach((aTagName) => {
-  if (!isTagClosed(indexMDFile, aTagName)) {
-    throw new Error(`Tag ${aTagName} has not been closed`);
-  }
-});
-
-/**
- * Checks to make sure that {::nomarkdown} doesn't miss {:/nomarkdown}
- * @param {string} textToSearch
- * @param {string} tagName
- */
-const throwIfMarkdownIsNotClosed = (textToSearch) => {
-  const openingTags = textToSearch.match(new RegExp(`{::nomarkdown}`, "g"));
-  const countOfOpeningTags = !!openingTags ? openingTags.length : 0;
-
-  const closingTags = textToSearch.match(new RegExp(`{:/nomarkdown}`, "g"));
-  const countOfClosingTags = !!closingTags ? closingTags.length : 0;
-
-  if (countOfOpeningTags !== countOfClosingTags) {
-    throw new Error(`Missing closing tags for nomarkdown`);
-  }
-};
-
-throwIfMarkdownIsNotClosed(indexMDFile);
-
-/**
- * Checks to make sure that _ is not an even number
- * @param {string} textToSearch
- */
-const throwIfItalicIsNotClosed = (textToSearch) => {
-  const italicTags = textToSearch.match(/\*/g);
-
-  const countOfItalicTags = !!italicTags ? italicTags.length : 0;
-
-  const isEven = countOfItalicTags % 2 === 0;
-
-  if (!isEven) {
-    throw new Error(
-      `An uneven number of italic tags (the * symbol) were found`
+  const getTextWithinATag = (htmlString) => {
+    return htmlString.substring(
+      htmlString.indexOf(">") + 1,
+      htmlString.lastIndexOf("<")
     );
-  }
-};
+  };
 
-throwIfItalicIsNotClosed(indexMDFile);
+  /**
+   *
+   * @param {string} textToSearch
+   * @param {string} tagName
+   */
+  const isTagClosed = (textToSearch, tagName) => {
+    const openingTags = textToSearch.match(new RegExp(`<${tagName}`, "g"));
+    const countOfOpeningTags = !!openingTags ? openingTags.length : 0;
 
-/**
- * Ensures that * or _ doesn't occur inside nomarkdown
- * @param {string} textToSearch
- */
-const throwIfHTMLContainsMarkdown = (textToSearch) => {
-  // Because of a peculiarity with .match, we'll do this test every time nomarkdown is used
-  const END_OF_NO_MARKDOWN = "{:/nomarkdown}";
-  const trials = textToSearch
-    .split(END_OF_NO_MARKDOWN)
-    .map((x) => x + END_OF_NO_MARKDOWN);
+    const closingTags = textToSearch.match(new RegExp(`<\/${tagName}`, "g"));
+    const countOfClosingTags = !!closingTags ? closingTags.length : 0;
 
-  trials.forEach((searchStr) => {
-    const sectionsOfNoMarkdown = searchStr.match(
-      /({::nomarkdown}).*({:\/nomarkdown})/g
-    );
+    return countOfOpeningTags === countOfClosingTags;
+  };
 
-    if (!sectionsOfNoMarkdown) {
-      return;
+  htmlTagsThatDoNotSelfClose.forEach((aTagName) => {
+    if (!isTagClosed(filenameToLint, aTagName)) {
+      throw new Error(`Tag ${aTagName} has not been closed`);
     }
+  });
 
-    sectionsOfNoMarkdown.forEach((chunk) => {
-      const markdownTokensToCheck = ["*", "_"];
+  /**
+   * Checks to make sure that {::nomarkdown} doesn't miss {:/nomarkdown}
+   * @param {string} textToSearch
+   * @param {string} tagName
+   */
+  const throwIfMarkdownIsNotClosed = (textToSearch) => {
+    const openingTags = textToSearch.match(new RegExp(`{::nomarkdown}`, "g"));
+    const countOfOpeningTags = !!openingTags ? openingTags.length : 0;
 
-      // check outside of the tags
-      markdownTokensToCheck.forEach((badToken) => {
-        const charBeforeEndOfNoMarkdown = chunk.substring(
-          chunk.indexOf(END_OF_NO_MARKDOWN) - 1
-        )[0];
-        if (charBeforeEndOfNoMarkdown === badToken) {
-          throw new Error(
-            `Can not have ("${badToken}") before the end of ${END_OF_NO_MARKDOWN} but it was there: ${chunk}`
-          );
-        }
-      });
+    const closingTags = textToSearch.match(new RegExp(`{:/nomarkdown}`, "g"));
+    const countOfClosingTags = !!closingTags ? closingTags.length : 0;
 
-      // check only the tag text
-      const textWithinATag = getTextWithinATag(chunk);
-      markdownTokensToCheck.forEach((badToken) => {
-        if (textWithinATag.includes(badToken)) {
-          throw new Error(
-            `Found markdown ("${badToken}") in this chunk: ${chunk}  ...specifically in the text of the tag ("${textWithinATag}")`
-          );
-        }
+    if (countOfOpeningTags !== countOfClosingTags) {
+      throw new Error(`Missing closing tags for nomarkdown`);
+    }
+  };
+
+  throwIfMarkdownIsNotClosed(filenameToLint);
+
+  /**
+   * Checks to make sure that _ is not an even number
+   * @param {string} textToSearch
+   */
+  const throwIfItalicIsNotClosed = (textToSearch) => {
+    const italicTags = textToSearch.match(/\*/g);
+
+    const countOfItalicTags = !!italicTags ? italicTags.length : 0;
+
+    const isEven = countOfItalicTags % 2 === 0;
+
+    if (!isEven) {
+      throw new Error(
+        `An uneven number of italic tags (the * symbol) were found`
+      );
+    }
+  };
+
+  throwIfItalicIsNotClosed(filenameToLint);
+
+  /**
+   * Ensures that * or _ doesn't occur inside nomarkdown
+   * @param {string} textToSearch
+   */
+  const throwIfHTMLContainsMarkdown = (textToSearch) => {
+    // Because of a peculiarity with .match, we'll do this test every time nomarkdown is used
+    const END_OF_NO_MARKDOWN = "{:/nomarkdown}";
+    const trials = textToSearch
+      .split(END_OF_NO_MARKDOWN)
+      .map((x) => x + END_OF_NO_MARKDOWN);
+
+    trials.forEach((searchStr) => {
+      const sectionsOfNoMarkdown = searchStr.match(
+        /({::nomarkdown}).*({:\/nomarkdown})/g
+      );
+
+      if (!sectionsOfNoMarkdown) {
+        return;
+      }
+
+      sectionsOfNoMarkdown.forEach((chunk) => {
+        const markdownTokensToCheck = ["*", "_"];
+
+        // check outside of the tags
+        markdownTokensToCheck.forEach((badToken) => {
+          const charBeforeEndOfNoMarkdown = chunk.substring(
+            chunk.indexOf(END_OF_NO_MARKDOWN) - 1
+          )[0];
+          if (charBeforeEndOfNoMarkdown === badToken) {
+            throw new Error(
+              `Can not have ("${badToken}") before the end of ${END_OF_NO_MARKDOWN} but it was there: ${chunk}`
+            );
+          }
+        });
+
+        // check only the tag text
+        const textWithinATag = getTextWithinATag(chunk);
+        markdownTokensToCheck.forEach((badToken) => {
+          if (textWithinATag.includes(badToken)) {
+            throw new Error(
+              `Found markdown ("${badToken}") in this chunk: ${chunk}  ...specifically in the text of the tag ("${textWithinATag}")`
+            );
+          }
+        });
       });
     });
+  };
+
+  throwIfHTMLContainsMarkdown(filenameToLint);
+
+  console.log(`END OF CUSTOM LINTER for filename ${filenameToLint}`);
+};
+
+const main = () => {
+  const fileNames = globSync("**/*.md", { ignore: "node_modules/**" });
+
+  const README_filename = "README.md";
+
+  fileNames.forEach((aFileToLint) => {
+    if (aFileToLint.includes(README_filename)) {
+      console.log(`Excluding ${README_filename}`);
+    } else {
+      lintAFile(aFileToLint);
+    }
   });
 };
 
-throwIfHTMLContainsMarkdown(indexMDFile);
-
-console.log("END OF CUSTOM LINTER");
+main();
